15df42a43ba2943c6b4bf0d6892e23531d2dedbd
Wei-Chung (Wells) Lu <34208509+wellslu@users.noreply.github.com>
2023-10-06 21:45:44 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,68 @@
+import time
+a = time.time()
+import pandas as pd
+import numpy as np
+
+key = input('要爬蟲選1，預測投組選2: ')
+if key == '1':
+    from tool_class import Tool
+    website = input('爬蟲網址 ex:https://www.rocketfinancial.com/Holdings.aspx?fID=250: ')
+    f13_data = Tool.crawler(website)
+    f13_data.to_csv('lone_pine.csv', index=False)
+elif key == '2':
+    import tool_class
+    f13_data = pd.read_csv('lone_pine.csv')
+    price_data = pd.read_csv('liquidity+highlow+class.csv', encoding='utf8', low_memory=False)
+    industry = pd.read_csv('industry2.csv')
+    asset = pd.read_csv('asset and book value.csv')
+
+    date = input('本月月份(結果為預期下個月的投組): ')
+    tool = tool_class.Tool(f13_data, price_data, industry, asset, date)
+
+    feature_data = tool.training_set()
+    b = time.time()
+    print(b-a)
+    testing_data = tool.testing_set()
+    b = time.time()
+    print(b-a)
+    last_buy_count = len(feature_data[feature_data['buy']==1])
+    not_buy_count = len(feature_data[feature_data['buy']==0])
+    start = last_buy_count+1
+
+    while start + last_buy_count * 3 < len(feature_data):
+        t_data = pd.concat([feature_data[:last_buy_count],feature_data[start:start+int(last_buy_count*1.5)]])
+        x = t_data[['volatility', 'liquidity', '52 week high', 'momentum', 'new_mom', 'new_52']].values
+        y = t_data[['buy']].values
+        x_test = testing_data[['volatility', 'liquidity', '52 week high', 'momentum', 'new_mom', 'new_52']].values
+        svc_clf = tool.svc_model(x, y)
+        if svc_clf is not False:
+            y_pred = svc_clf.predict(x_test)
+            testing_data.loc[:, 'svc'] = y_pred
+            testing_data = testing_data[testing_data['svc']==1]
+        x_test = testing_data[['volatility', 'liquidity', '52 week high', 'momentum', 'new_mom', 'new_52']].values
+        rft_clf = tool.rft_model(x, y)
+        if rft_clf is not False:
+            y_pred = rft_clf.predict(x_test)
+            testing_data.loc[:, 'rtf'] = y_pred
+            testing_data = testing_data[testing_data['rtf']==1]
+        start = start+int(last_buy_count*1.5)
+    t_data = pd.concat([feature_data[:last_buy_count],feature_data[start:]])
+    x = t_data[['volatility', 'liquidity', '52 week high', 'momentum', 'new_mom', 'new_52']].values
+    y = t_data[['buy']].values
+    x_test = testing_data[['volatility', 'liquidity', '52 week high', 'momentum', 'new_mom', 'new_52']].values
+    svc_clf = tool.svc_model(x, y)
+    if svc_clf is not False:
+        y_pred = svc_clf.predict(x_test)
+        testing_data.loc[:, 'svc'] = y_pred
+        testing_data = testing_data[testing_data['svc']==1]
+    x_test = testing_data[['volatility', 'liquidity', '52 week high', 'momentum', 'new_mom', 'new_52']].values
+    rft_clf = tool.rft_model(x, y)
+    if rft_clf is not False:
+        y_pred = rft_clf.predict(x_test)
+        testing_data.loc[:, 'rtf'] = y_pred
+        testing_data = testing_data[testing_data['rtf']==1]
+
+    print(list(testing_data['ticker']))
+    
+b = time.time()
+print(b-a)
